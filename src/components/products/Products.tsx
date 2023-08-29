7c0cf9faecdcbfa88c47ae746aec35dfba0e398a
mehdi <mehdisamiyann@gmail.com>
2023-08-29 23:53:00 +0330

    add product card

--- src/components/products/Products.tsx
+++ src/components/products/Products.tsx
@@ -1,32 +1,146 @@
-import React from 'react'
+import image from '../../assets/image/product.jpg'
+import addToCart from '../../assets/svg/addtocardicon.svg'
 import './products.scss'
+
 function Products() {
   return (
-  <main>
-    <h1>محصولات</h1>
-    <ul >
+    <main>
+      <h1>محصولات</h1>
+      <ul >
       <li>
-        <div>
-          
-        </div>
-      </li>
-      <li>
-       <img />
-      </li>
-      <li>
-        sadasdasd
-      </li>
-      <li>
-        sadasdasd
-      </li>
-      <li>
-        sadasdasd
-      </li>
-      <li>
-        sadasdasd
-      </li>
-    </ul>
-  </main>
+          <div className='product'>
+            <img src={image} className='image-of-product' />
+            <p className="title-of-product">شامپو تخصصی ضد ریزش خشک و نرمال</p>
+            <p className="product-size">200 میل</p>
+            <div className="product-footer ">
+              <img className='product-icon ' src={addToCart} />
+              <p className="product-price">158,000<span className="toman">تومان</span></p>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div className='product'>
+            <img src={image} className='image-of-product' />
+            <p className="title-of-product">شامپو تخصصی ضد ریزش خشک و نرمال</p>
+            <p className="product-size">200 میل</p>
+            <div className="product-footer ">
+              <img className='product-icon ' src={addToCart} />
+              <p className="product-price">158,000<span className="toman">تومان</span></p>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div className='product'>
+            <img src={image} className='image-of-product' />
+            <p className="title-of-product">شک و نرمال</p>
+            <p className="product-size">200 میل</p>
+            <div className="product-footer ">
+              <img className='product-icon ' src={addToCart} />
+              <p className="product-price">158,000<span className="toman">تومان</span></p>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div className='product'>
+            <img src={image} className='image-of-product' />
+            <p className="title-of-product">شامپو تخصصی ضد ؤسشیشسیشسیشسیشسیریزشasdasdasdasdadsasd خشک و نرمال</p>
+            <p className="product-size">200 میل</p>
+            <div className="product-footer ">
+              <img className='product-icon ' src={addToCart} />
+              <p className="product-price">158,000<span className="toman">تومان</span></p>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div className='product'>
+            <img src={image} className='image-of-product' />
+            <p className="title-of-product">شامپو تخصصی ضد ریزش خشک و نرمال</p>
+            <p className="product-size">200 میل</p>
+            <div className="product-footer ">
+              <img className='product-icon ' src={addToCart} />
+              <p className="product-price">158,000<span className="toman">تومان</span></p>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div className='product'>
+            <img src={image} className='image-of-product' />
+            <p className="title-of-product">شامپو تخصصی ضد ریزش خشک و نرمال</p>
+            <p className="product-size">200 میل</p>
+            <div className="product-footer ">
+              <img className='product-icon ' src={addToCart} />
+              <p className="product-price">158,000<span className="toman">تومان</span></p>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div className='product'>
+            <img src={image} className='image-of-product' />
+            <p className="title-of-product">شامپو تخصصی ضد ریزش خشک و نرمال</p>
+            <p className="product-size">200 میل</p>
+            <div className="product-footer ">
+              <img className='product-icon ' src={addToCart} />
+              <p className="product-price">158,000<span className="toman">تومان</span></p>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div className='product'>
+            <img src={image} className='image-of-product' />
+            <p className="title-of-product">شامپو تخصصی ضد ریزش خشک و نرمال</p>
+            <p className="product-size">200 میل</p>
+            <div className="product-footer ">
+              <img className='product-icon ' src={addToCart} />
+              <p className="product-price">158,000<span className="toman">تومان</span></p>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div className='product'>
+            <img src={image} className='image-of-product' />
+            <p className="title-of-product">شامپو تخصصی ضد ریزش خشک و نرمال</p>
+            <p className="product-size">200 میل</p>
+            <div className="product-footer ">
+              <img className='product-icon ' src={addToCart} />
+              <p className="product-price">158,000<span className="toman">تومان</span></p>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div className='product'>
+            <img src={image} className='image-of-product' />
+            <p className="title-of-product">شامپو تخصصی ضد ریزش خشک و نرمال</p>
+            <p className="product-size">200 میل</p>
+            <div className="product-footer ">
+              <img className='product-icon ' src={addToCart} />
+              <p className="product-price">158,000<span className="toman">تومان</span></p>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div className='product'>
+            <img src={image} className='image-of-product' />
+            <p className="title-of-product">شامپو تخصصی ضد ریزش خشک و نرمال</p>
+            <p className="product-size">200 میل</p>
+            <div className="product-footer ">
+              <img className='product-icon ' src={addToCart} />
+              <p className="product-price">158,000<span className="toman">تومان</span></p>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div className='product'>
+            <img src={image} className='image-of-product' />
+            <p className="title-of-product">شامپو تخصصی ضد ریزش خشک و نرمال</p>
+            <p className="product-size">200 میل</p>
+            <div className="product-footer ">
+              <img className='product-icon ' src={addToCart} />
+              <p className="product-price">158,000<span className="toman">تومان</span></p>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </main>
   )
 }
 
